2f2192dc64cdc56c1d18452378088c508c548397
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-22 17:46:55 +0000

    update test library

--- tests/route.test.js
+++ tests/route.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from "./components/App";
 import {store} from "./store";
@@ -7,13 +7,13 @@ import { Provider } from "react-redux";
 
 describe('404 Page', () => {
   test('renders 404 message', () => {
-    const wrapper = mount(
+    const { getByText } = render(
       <Provider store={store}>
       <MemoryRouter initialEntries={['/product']}>
         <App />
       </MemoryRouter>
       </Provider>
     );
-    expect(wrapper.text()).toMatch(/page not found/);
+    expect(getByText('Page Not Found')).toBeInTheDocument();
   });
 });
