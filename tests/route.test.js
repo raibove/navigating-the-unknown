7f683041b6a5918ae39ed1f16cc89398beb80626
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-22 16:33:37 +0000

    fix test

--- tests/route.test.js
+++ tests/route.test.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import App from "./components/App";
+import {store} from "./store";
+import { Provider } from "react-redux";
 
 describe('404 Page', () => {
   test('renders 404 message', () => {
     const wrapper = mount(
+      <Provider store={store}>
       <MemoryRouter initialEntries={['/product']}>
         <App />
       </MemoryRouter>
+      </Provider>
     );
     expect(wrapper.text()).toEqual("Oops, Looks like you've stumbled upon a item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!");
   });
